6e278baf8568caea8bcfad759ab5e3bab0e4dcb5
VictorMeneghini <vhmeneghini@gmail.com>
2018-12-14 00:04:24 -0300

    Add router call to inside vue

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
 import theme from './theme'
+import router from './router';
 import 'vuetify/dist/vuetify.css'
 
-Vue.use(Vuetify,{ theme })
+Vue.use(Vuetify,{ theme });
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
