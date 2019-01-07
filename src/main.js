1022c96059e0016d887567bc840ee81649016666
VictorMeneghini <vhmeneghini@gmail.com>
2019-01-06 23:52:01 -0300

    Add store to main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import Vuetify from 'vuetify'
 import theme from './theme'
 import router from './router';
+import {store} from './store';
+
 import 'vuetify/dist/vuetify.css'
 
 Vue.use(Vuetify,{ theme });
@@ -10,5 +12,6 @@ Vue.use(Vuetify,{ theme });
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
