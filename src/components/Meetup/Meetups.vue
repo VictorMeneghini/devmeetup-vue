13f3dd8d4cd00db06d8ed8a4c0c70dc5ea5d6015
VictorMeneghini <vhmeneghini@gmail.com>
2019-01-06 23:53:08 -0300

    Change hardedcode to Vuex getters by state

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,25 +1,30 @@
 <template>
   <v-container>
-    <v-layout row wrap>
+    <v-layout
+      row
+      wrap
+      v-for="meetup in meetups"
+      :key="meetup.id"
+      class="mb-2">
       <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
         <v-card class="info">
           <v-container fluid>
             <v-layout row>
               <v-flex xs5 sm4 md3>
                 <v-img
-                  src="https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg"
+                  :src="meetup.imageUrl"
                   height="130px"
                 />
               </v-flex>
               <v-flex xs7 sm8 md9>
                 <v-card-title primary-title>
                   <div>
-                    <h5 class="white--text mb-0">My Meetup</h5>
-                    <div>17th July 2017</div>
+                    <h2 class="white--text mb-0">{{meetup.title}}</h2>
+                    <div>{{meetup.date}}</div>
                   </div>
                 </v-card-title>
                 <v-card-actions>
-                  <v-btn flat to="/meetups/1">
+                  <v-btn flat :to="'/meetups/' + meetup.id">
                     <v-icon left light>arrow_forward</v-icon>
                     View Meetup
                   </v-btn>
@@ -35,7 +40,12 @@
 
 <script>
 export default {
-  name: "Meetups"
+  name: "Meetups",
+  computed: {
+    meetups() {
+     return this.$store.getters.loadedMeetups;
+    }
+  }
 };
 </script>
 
